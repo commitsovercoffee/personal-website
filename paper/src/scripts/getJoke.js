8ef95c1e682fc3f7fae27b2a5cb1e5f492a6e449
Sourav Singh <commitsovercoffee@gmail.com>
2023-05-08 19:42:32 +0530

    revamping website: fresh start, new approach

--- paper/src/scripts/getJoke.js
+++
@@ -1,9 +0,0 @@
-     window.onload = function () {
-        fetch("https://official-joke-api.appspot.com/random_joke")
-            .then((response) => response.json())
-            .then((data) => {
-                const jokeElement = document.getElementById("joke");
-                jokeElement.innerHTML = `${data.setup} ${data.punchline}`;
-            });
-    };
-
