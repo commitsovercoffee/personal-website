4bdf3248d44948e50bf554d0fc9397bc3fcdb3e0
Sourav Singh <commitsovercoffee@gmail.com>
2023-06-14 16:56:36 +0530

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-require('@tailwindcss/typography'),
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Contrail One", ...defaultTheme.fontFamily.sans],
+                serif: ["Open Sans", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
     ],
 }
