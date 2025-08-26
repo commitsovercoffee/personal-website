4ad02a08458845d2a5d74c85f0a8c8f07ca86a37
commitsovercoffee <commitsovercoffee@gmail.com>
2025-08-27 03:34:01 +0530

    fixes width & scroll transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,20 +6,25 @@
 	import { fly } from 'svelte/transition';
 	import { cubicIn, cubicOut } from 'svelte/easing';
 	let { data, children } = $props();
+
+	import { navigating } from '$app/state';
+	import { page } from '$app/state';
 </script>
 
 <div
 	class="{theme.prefers} flex min-h-screen flex-col justify-between bg-bg transition-colors duration-300"
 >
 	<Nav />
-	{#key data.url}
-		<div
-			in:fly={{ easing: cubicOut, y: 40, duration: 300, delay: 300 }}
-			out:fly={{ easing: cubicIn, y: -40, duration: 300 }}
-			class="mx-auto my-40 prose px-2 text-fg md:px-0"
-		>
-			{@render children?.()}
-		</div>
-	{/key}
+	<div>
+		{#if !navigating.to}
+			<div
+				in:fly={{ easing: cubicOut, y: 40, duration: 300, delay: 300 }}
+				out:fly={{ easing: cubicIn, y: -40, duration: 300 }}
+				class="mx-auto my-40 prose px-2 text-fg md:px-0"
+			>
+				{@render children?.()}
+			</div>
+		{/if}
+	</div>
 	<Footer />
 </div>
