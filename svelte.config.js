be6cdc035dde103978211e3298daf9e08d777bd3
commitsovercoffee <commitsovercoffee@gmail.com>
2025-08-26 22:41:04 +0530

    adds syntax highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,21 @@
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import { createHighlighter } from 'shiki';
 import adapter from '@sveltejs/adapter-static';
 
+const theme = 'github-dark';
+const highlighter = await createHighlighter({
+	themes: [theme],
+	langs: ['javascript', 'typescript', 'go', 'bash', 'json', 'svelte', 'diff', 'md']
+});
+
 const mdsvexOptions = {
-	extensions: ['.md']
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme }));
+			return `{@html \`${html}\` }`;
+		}
+	}
 };
 
 /** @type {import('@sveltejs/kit').Config} */
