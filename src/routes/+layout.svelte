4159c326fb76d7b5aca8bb3bab928224ad324396
commitsovercoffee <commitsovercoffee@gmail.com>
2025-08-26 22:24:25 +0530

    updates favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,11 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import Footer from '$lib/components/Footer.svelte';
 	import Nav from '$lib/components/Nav.svelte';
 	import { theme } from '$lib/shared.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div
 	class="{theme.prefers} flex min-h-screen flex-col justify-between bg-bg transition-colors duration-300"
